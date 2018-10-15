05ba0499df0f20d467fc6baa63703709a58da20f
vishal-agarwal-adt <vishal.agarwal@absolutdata.com>
2018-10-15 17:49:59 +0530

    Code changes for mysql 8

--- src/main/java/com/adt/sample/devops/config/AppConfig.java
+++ src/main/java/com/adt/sample/devops/config/AppConfig.java
@@ -29,41 +29,6 @@ public class AppConfig {
 	@Autowired
 	private Environment env;
 
-	/*
-	 * @Bean public LocalSessionFactoryBean getSessionFactory() {
-	 * LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-	 * 
-	 * Properties props = new Properties(); // Setting JDBC properties
-	 * props.put(DRIVER, env.getProperty("mysql.driver")); props.put(URL,
-	 * env.getProperty("mysql.url")); props.put(USER,
-	 * env.getProperty("mysql.user")); props.put(PASS,
-	 * env.getProperty("mysql.password")); props.put(DIALECT,
-	 * env.getProperty("mysql.dialect"));
-	 * 
-	 * // Setting Hibernate properties props.put(SHOW_SQL,
-	 * env.getProperty("hibernate.show_sql")); props.put(HBM2DDL_AUTO,
-	 * env.getProperty("hibernate.hbm2ddl.auto"));
-	 * 
-	 * // Setting C3P0 properties props.put(C3P0_MIN_SIZE,
-	 * env.getProperty("hibernate.c3p0.min_size")); props.put(C3P0_MAX_SIZE,
-	 * env.getProperty("hibernate.c3p0.max_size"));
-	 * props.put(C3P0_ACQUIRE_INCREMENT,
-	 * env.getProperty("hibernate.c3p0.acquire_increment")); props.put(C3P0_TIMEOUT,
-	 * env.getProperty("hibernate.c3p0.timeout")); props.put(C3P0_MAX_STATEMENTS,
-	 * env.getProperty("hibernate.c3p0.max_statements"));
-	 * 
-	 * factoryBean.setHibernateProperties(props);
-	 * factoryBean.setPackagesToScan("com.adt.sample.devops.model");
-	 * 
-	 * return factoryBean; }
-	 * 
-	 * @Bean public HibernateTransactionManager getTransactionManager() {
-	 * HibernateTransactionManager transactionManager = new
-	 * HibernateTransactionManager();
-	 * transactionManager.setSessionFactory(getSessionFactory().getObject()); return
-	 * transactionManager; }
-	 */
-
 	@Bean
 	public LocalSessionFactoryBean sessionFactory() {
 		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
